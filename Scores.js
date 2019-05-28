9af0e036b5ed1b9e157329af8137a378c1209409
zion-fung <36176037+zion-fung@users.noreply.github.com>
2019-05-27 23:48:41 -0700

    added scores activity

--- Scores.js
+++ Scores.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react"
-import { Grid, Row, Col } from "react-native-easy-grid"
+import { View, StyleSheet } from "react-native"
 import { Text } from "react-native-elements"
 import { getMaxStreak, getStreak } from "./storage/storage"
+import LinearGradient from "react-native-linear-gradient";
 
 export default class Scores extends Component {
     constructor(props) {
@@ -22,26 +23,68 @@ export default class Scores extends Component {
         const quotes = await getStreak(2)
         const scores = [grid, pictures, quotes]
         console.log("scores:", scores);
-        this.setState({maxStreaks: maxStreaks, currentStreaks: scores})
+        this.setState({ maxStreaks: maxStreaks, currentStreaks: scores })
+    }
+    static navigationOptions = {
+        headerTintColor: "white",
+        headerStyle: { backgroundColor:"#2089dc" },
+        headerTitle: "High Scores"
     }
     render() {
-        return(
-            <Grid>
-                <Row size={1}></Row>
-                <Row size={2}>
-                    <Col size={1}></Col>
-                    <Col size={5}>
-                        <Text>Grid longest streak: {this.state.maxStreaks[0]}</Text>
-                        <Text>Grid current streak: {this.state.currentStreaks[0]}</Text>
-                        <Text>Pictures longest streak: {this.state.maxStreaks[1]}</Text>
-                        <Text>Pictures current streak: {this.state.currentStreaks[1]}</Text>
-                        <Text>Quotes longest streak: {this.state.maxStreaks[2]}</Text>
-                        <Text>Quotes current streak: {this.state.currentStreaks[2]}</Text>
-                    </Col>
-                    <Col size={1}></Col>
-                </Row>
-                <Row size={1}></Row>
-            </Grid>
+        return (
+            <LinearGradient colors={["lightcoral", "lightyellow"]}>
+                <View style={{ height: "100%" }}>
+                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    </View>
+                    <View style={{ flex: 3 }}>
+                        <View style={{ flex: 5 }}>
+                            <View style={{ flex: 1, flexDirection: "row", justifyContent: "center", alignSelf: "center" }}>
+                                <View>
+                                    <View style={styles.space}>
+                                        <Text h3>Grid</Text>
+                                        <Text h4>longest streak:</Text>
+                                        <Text h4>current streak:</Text>
+                                    </View>
+                                    <View style={styles.space}>
+                                        <Text h3>Pictures</Text>
+                                        <Text h4>longest streak:</Text>
+                                        <Text h4>current streak:</Text>
+                                    </View>
+                                    <View style={styles.space}>
+                                        <Text h3>Quotes</Text>
+                                        <Text h4>longest streak:</Text>
+                                        <Text h4>current streak:</Text>
+                                    </View>
+                                </View>
+                                <View style={{ marginLeft: 50 }}>
+                                    <View style={styles.space}>
+                                        <Text h3></Text>
+                                        <Text h4>{this.state.maxStreaks[0]}</Text>
+                                        <Text h4>{this.state.currentStreaks[0]}</Text>
+                                    </View>
+                                    <View style={styles.space}>
+                                        <Text h3></Text>
+                                        <Text h4>{this.state.maxStreaks[1]}</Text>
+                                        <Text h4>{this.state.currentStreaks[1]}</Text>
+                                    </View>
+                                    <View style={styles.space}>
+                                        <Text h3></Text>
+                                        <Text h4>{this.state.maxStreaks[2]}</Text>
+                                        <Text h4>{this.state.currentStreaks[2]}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flex: 1 }}></View>
+                </View>
+            </LinearGradient>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    space: {
+        marginBottom: 15
+    }
+})
